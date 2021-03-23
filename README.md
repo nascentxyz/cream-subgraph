5dcfc8cd8f3d77fab0ec919e0f13ea0902cbf0e2
Andreas Bigger <abigger87@gmail.com>
2021-03-23 12:19:34 -0700

    initial port from cream fi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cream-subgraph
+
 Subgraph for Cream Finance
+
+[Cream Subgraph](https://thegraph.com/explorer/subgraph/abigger87/cream-subgraph)
---
+++ src/mappings/markets.ts
@@ -0,0 +1,222 @@
+/* eslint-disable prefer-const */ // to satisfy AS compiler
+
+// For each division by 10, add one to exponent to truncate one significant figure
+import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts'
+import { Market, Comptroller } from '../types/schema'
+import { PriceOracle } from '../types/templates/CToken/PriceOracle'
+import { ERC20 } from '../types/templates/CToken/ERC20'
+import { CToken } from '../types/templates/CToken/CToken'
+
+import {
+  exponentToBigDecimal,
+  mantissaFactor,
+  mantissaFactorBD,
+  cTokenDecimalsBD,
+  zeroBD,
+} from './helpers'
+
+let cUSDCAddress = '0x44fbebd2f576670a6c33f6fc0b00aa8c5753b322'
+let cETHAddress = '0xd06527d5e56a3495252a528c4987003b712860ee'
+
+// Used for all cERC20 contracts
+function getTokenPrice(
+  eventAddress: Address,
+  underlyingAddress: Address,
+  underlyingDecimals: i32,
+): BigDecimal {
+  let comptroller = Comptroller.load('1')
+  let oracleAddress = comptroller.priceOracle as Address
+  let underlyingPrice: BigDecimal
+
+  let mantissaDecimalFactor = 18 - underlyingDecimals + 18
+  let bdFactor = exponentToBigDecimal(mantissaDecimalFactor)
+  let oracle = PriceOracle.bind(oracleAddress)
+  underlyingPrice = oracle
+    .getUnderlyingPrice(eventAddress)
+    .toBigDecimal()
+    .div(bdFactor)
+
+  return underlyingPrice
+}
+
+// Returns the price of USDC in eth. i.e. 0.005 would mean ETH is $200
+function getUSDCpriceETH(): BigDecimal {
+  let comptroller = Comptroller.load('1')
+  let oracleAddress = comptroller.priceOracle as Address
+  let usdPrice: BigDecimal
+
+  // See notes on block number if statement in getTokenPrices()
+  let oracle = PriceOracle.bind(oracleAddress)
+  let mantissaDecimalFactorUSDC = 18 - 6 + 18
+  let bdFactorUSDC = exponentToBigDecimal(mantissaDecimalFactorUSDC)
+  usdPrice = oracle
+    .getUnderlyingPrice(Address.fromString(cUSDCAddress))
+    .toBigDecimal()
+    .div(bdFactorUSDC)
+  return usdPrice
+}
+
+export function createMarket(marketAddress: string): Market {
+  let market: Market
+  let contract = CToken.bind(Address.fromString(marketAddress))
+
+  // It is CETH, which has a slightly different interface
+  if (marketAddress == cETHAddress) {
+    market = new Market(marketAddress)
+    market.underlyingAddress = Address.fromString(
+      '0x0000000000000000000000000000000000000000',
+    )
+    market.underlyingDecimals = 18
+    market.underlyingPrice = BigDecimal.fromString('1')
+    market.underlyingName = 'Ether'
+    market.underlyingSymbol = 'ETH'
+    market.underlyingPriceUSD = zeroBD
+    // It is all other CERC20 contracts
+  } else {
+    market = new Market(marketAddress)
+    market.underlyingAddress = contract.underlying()
+    let underlyingContract = ERC20.bind(market.underlyingAddress as Address)
+    market.underlyingDecimals = underlyingContract.decimals()
+    market.underlyingName = underlyingContract.name()
+    market.underlyingSymbol = underlyingContract.symbol()
+    market.underlyingPriceUSD = zeroBD
+    market.underlyingPrice = zeroBD
+    if (marketAddress == cUSDCAddress) {
+      market.underlyingPriceUSD = BigDecimal.fromString('1')
+    }
+  }
+
+  let interestRateModelAddress = contract.try_interestRateModel()
+  let reserveFactor = contract.try_reserveFactorMantissa()
+
+  market.borrowRate = zeroBD
+  market.cash = zeroBD
+  market.collateralFactor = zeroBD
+  market.exchangeRate = zeroBD
+  market.interestRateModelAddress = interestRateModelAddress.reverted
+    ? Address.fromString('0x0000000000000000000000000000000000000000')
+    : interestRateModelAddress.value
+  market.name = contract.name()
+  market.reserves = zeroBD
+  market.supplyRate = zeroBD
+  market.symbol = contract.symbol()
+  market.totalBorrows = zeroBD
+  market.totalSupply = zeroBD
+
+  market.accrualBlockNumber = 0
+  market.blockTimestamp = 0
+  market.borrowIndex = zeroBD
+  market.reserveFactor = reserveFactor.reverted ? BigInt.fromI32(0) : reserveFactor.value
+
+  return market
+}
+
+export function updateMarket(
+  marketAddress: Address,
+  blockNumber: i32,
+  blockTimestamp: i32,
+): Market {
+  let marketID = marketAddress.toHexString()
+  let market = Market.load(marketID)
+  if (market == null) {
+    market = createMarket(marketID)
+  }
+
+  // Only updateMarket if it has not been updated this block
+  if (market.accrualBlockNumber != blockNumber) {
+    let contractAddress = Address.fromString(market.id)
+    let contract = CToken.bind(contractAddress)
+
+    let usdPriceInEth = getUSDCpriceETH()
+
+    // if cETH, we only update USD price
+    if (market.id == cETHAddress) {
+      market.underlyingPriceUSD = market.underlyingPrice
+        .div(usdPriceInEth)
+        .truncate(market.underlyingDecimals)
+    } else {
+      let tokenPriceEth = getTokenPrice(
+        contractAddress,
+        market.underlyingAddress as Address,
+        market.underlyingDecimals,
+      )
+      market.underlyingPrice = tokenPriceEth.truncate(market.underlyingDecimals)
+      // if USDC, we only update ETH price
+      if (market.id != cUSDCAddress) {
+        market.underlyingPriceUSD = market.underlyingPrice
+          .div(usdPriceInEth)
+          .truncate(market.underlyingDecimals)
+      }
+    }
+
+    market.accrualBlockNumber = contract.accrualBlockNumber().toI32()
+    market.blockTimestamp = blockTimestamp
+    market.totalSupply = contract
+      .totalSupply()
+      .toBigDecimal()
+      .div(cTokenDecimalsBD)
+
+    /* Exchange rate explanation
+       In Practice
+        - If you call the cDAI contract on etherscan it comes back (2.0 * 10^26)
+        - If you call the cUSDC contract on etherscan it comes back (2.0 * 10^14)
+        - The real value is ~0.02. So cDAI is off by 10^28, and cUSDC 10^16
+       How to calculate for tokens with different decimals
+        - Must div by tokenDecimals, 10^market.underlyingDecimals
+        - Must multiply by ctokenDecimals, 10^8
+        - Must div by mantissa, 10^18
+     */
+    market.exchangeRate = contract
+      .exchangeRateStored()
+      .toBigDecimal()
+      .div(exponentToBigDecimal(market.underlyingDecimals))
+      .times(cTokenDecimalsBD)
+      .div(mantissaFactorBD)
+      .truncate(mantissaFactor)
+    market.borrowIndex = contract
+      .borrowIndex()
+      .toBigDecimal()
+      .div(mantissaFactorBD)
+      .truncate(mantissaFactor)
+
+    market.reserves = contract
+      .totalReserves()
+      .toBigDecimal()
+      .div(exponentToBigDecimal(market.underlyingDecimals))
+      .truncate(market.underlyingDecimals)
+    market.totalBorrows = contract
+      .totalBorrows()
+      .toBigDecimal()
+      .div(exponentToBigDecimal(market.underlyingDecimals))
+      .truncate(market.underlyingDecimals)
+    market.cash = contract
+      .getCash()
+      .toBigDecimal()
+      .div(exponentToBigDecimal(market.underlyingDecimals))
+      .truncate(market.underlyingDecimals)
+
+    // Must convert to BigDecimal, and remove 10^18 that is used for Exp in Compound Solidity
+    market.borrowRate = contract
+      .borrowRatePerBlock()
+      .toBigDecimal()
+      .times(BigDecimal.fromString('2102400'))
+      .div(mantissaFactorBD)
+      .truncate(mantissaFactor)
+
+    // This fails on only the first call to cZRX. It is unclear why, but otherwise it works.
+    // So we handle it like this.
+    let supplyRatePerBlock = contract.try_supplyRatePerBlock()
+    if (supplyRatePerBlock.reverted) {
+      log.info('***CALL FAILED*** : cERC20 supplyRatePerBlock() reverted', [])
+      market.supplyRate = zeroBD
+    } else {
+      market.supplyRate = supplyRatePerBlock.value
+        .toBigDecimal()
+        .times(BigDecimal.fromString('2102400'))
+        .div(mantissaFactorBD)
+        .truncate(mantissaFactor)
+    }
+    market.save()
+  }
+  return market as Market
+}
